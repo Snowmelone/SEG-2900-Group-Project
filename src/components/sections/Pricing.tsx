119fe13d1c44b537fdfd7c25a00d909334079e73
Snowmelone <Kskhazabi@yahoo.com>
2024-10-27 20:09:03 -0400

    Add files via upload

--- src/components/sections/Pricing.tsx
+++ src/components/sections/Pricing.tsx
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from 'react'
 
 import '../gradients.css'
 import '../hero.css'
-
-
+import partsInfo from '@/data/partsData'
+import PartsGrid from '../library/partsGrid'
 import { Jura } from 'next/font/google'
 import { IoIosMenu, IoMdClose } from 'react-icons/io'
 const jura = Jura({
@@ -12,7 +12,11 @@ const jura = Jura({
     subsets: ['latin'],
 
 })
+const juraBold = Jura({
+    weight: '700',
+    subsets: ['latin'],
 
+})
 
 export default function Pricing({
     language = "en"
@@ -34,10 +38,66 @@ export default function Pricing({
 
 
     return (
-        <div id='Pricing' className='flex justify-center items-center h-[100dvh] z-50  left-0 top-0 w-full'>
-           <button>pricing btn</button>
+        <div id='#' className='flex flex-col justify-between items-center  z-50 h-auto  left-0 top-0 w-full '>
+            {/* Gucci Collab */}
 
-         
+            <Card className='grid md:grid-cols-3 grid-cols-1 justify-center items-center w-full max-h-[400px] h-full bg-white'>
+                <div className={` flex flex-col justify-center space-y-4 text-center text-5xl bg-gray-50 h-full items-center ${jura.className}`}>
+            {/* <div className='absolute z-10'>
+                <Image className='object-cover  w-full h-full' src='gucciwearer.png'/>
+
+            </div> */}
+                    <div className='z-20 '>
+                        Different
+                    </div>
+                    <div>
+                        Frame
+                    </div>
+                
+                    <div className='z-20 font-bold  text-gradient-purple '>
+                        Better Game
+                    </div>
+                  
+                </div>
+                <div className='flex flex-col justify-between py-2 h-full items-center'>
+                    <div className='text-4xl font-bold text-center'>
+                        <div className={`${jura.className}`}>
+                            Gucci x Lens
+                        </div>
+                        <div className='text-xl text-gray-500 font-semibold'>
+                            Innovative Style
+
+                        </div>
+                        <div className='text-lg text-gray-400 font-normal'>
+                            299$
+
+                        </div>
+                    </div>
+                    <div className=''>
+                        <Image className='h-48' src='gucciex_.jpg' />
+                    </div>
+
+                </div>
+                <div className='flex justify-center h-full items-center bg-black text-[#ffbf00]'>
+                    coming xx.xx.xxxx
+                </div>
+            </Card>
+            {/* Parts Grid */}
+            <div className='flex flex-col items-center w-full h-full justify-center py-16 pt-32'>
+                <div className={`text-5xl font-bold ${jura.className} pb-6`}>
+                    Parts
+                </div>
+                <div className='w-full h-full flex justify-between px-16'>
+                <div className='w-3/12  bg-gray-100'>
+                    x
+                </div>
+                <div className='w-9/12 flex items-center '>
+
+                <PartsGrid partsInfo={partsInfo} />
+                </div>
+                    </div>
+            </div>
+
         </div >
     )
 }
